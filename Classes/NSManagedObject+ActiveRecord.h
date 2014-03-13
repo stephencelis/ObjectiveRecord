bce36ad63117a78d37c8c78b85b8fb3b3487493c
Stephen Celis <stephen@stephencelis.com>
2014-03-12 19:44:02 -0700

    Document +[NSManagedObjectContext defaultContext]
    Signed-off-by: Stephen Celis <stephen@stephencelis.com>

--- Classes/NSManagedObject+ActiveRecord.h
+++ Classes/NSManagedObject+ActiveRecord.h
@@ -25,7 +25,16 @@
 #import "CoreDataManager.h"
 
 @interface NSManagedObjectContext (ActiveRecord)
+
+/**
+ The default context (as defined on the @c CoreDataManager singleton).
+
+ @see -[CoreDataManager managedObjectContext]
+
+ @return A managed object context.
+ */
 + (NSManagedObjectContext *)defaultContext;
+
 @end
 
 @interface NSManagedObject (ActiveRecord)
